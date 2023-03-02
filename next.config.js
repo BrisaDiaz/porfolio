4225c3a9e359238faf9f8805823c1b8a2faeb983
Brisa Díaz <brisaabigaildiaz2000@gmail.com>
2023-03-02 08:28:21 -0300

    fix: disable eslint no require var use for @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
-import WithBundleAnalyzer from '@next/bundle-analyzer'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
 }
-export default WithBundleAnalyzer({
-    enabled: process.env.ANALYZE === 'true',
-})(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig)
